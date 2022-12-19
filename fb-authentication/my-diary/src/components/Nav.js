a11c28470e8e88c142357158a025a7b3c115ddf3
greenT-Hee <heehee970322@gmail.com>
2022-12-20 00:43:49 +0900

    fb-lecture setting

---
+++ fb-authentication/my-diary/src/components/Nav.js
@@ -0,0 +1,15 @@
+import styles from './Nav.module.css'
+import { Link } from 'react-router-dom'
+
+export default function Nav () {
+
+    return (
+        <nav className={styles.nav}>
+            <h1 className={styles.title}>비밀일기 🏰</h1>
+            <ul className={styles.list_nav}>
+                <li><Link to='/login'>로그인</Link></li>
+                <li><Link to='/signup'>회원 가입</Link></li>
+            </ul>
+        </nav>
+    )
+}
