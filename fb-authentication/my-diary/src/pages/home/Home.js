20c92e71a693320ac8d814224b006c6f062f9b4c
greenT-Hee <heehee970322@gmail.com>
2022-12-20 16:17:33 +0900

    홈화면 작성

--- fb-authentication/my-diary/src/pages/home/Home.js
+++ fb-authentication/my-diary/src/pages/home/Home.js
@@ -1,7 +1,16 @@
 import styles from './Home.module.css';
+import DiaryForm from './DiaryForm';
 
 export default function Home () {
     return (
-        <div>home</div>
+        <main className={styles.cont}>
+            <aside className={styles.side_menu}>
+                <DiaryForm />
+            </aside>
+            <ul className={styles.content_list}>
+                dairy list
+            </ul>
+        </main>
+
     )
 }
