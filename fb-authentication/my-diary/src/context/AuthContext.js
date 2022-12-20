4d25041a619bc8b0e3dd97c7be11b18fc645aab5
greenT-Hee <heehee970322@gmail.com>
2022-12-20 15:45:21 +0900

    화면 이동 라우터 설정 및 새로고침 문제 해결

--- fb-authentication/my-diary/src/context/AuthContext.js
+++ fb-authentication/my-diary/src/context/AuthContext.js
@@ -1,4 +1,6 @@
-import { createContext, useReducer } from 'react'; 
+import { onAuthStateChanged } from 'firebase/auth';
+import { createContext, useEffect, useReducer } from 'react'; 
+import { appAuth } from '../firebase/config';
 
 // context 객첵를 생성
 const AuthContext = createContext();
@@ -10,16 +12,26 @@ const authReducer = (state, action) => {
             return {...state, user : action.payload}
         case 'logout' :
             return {...state, user : null}
+        case 'isAuthReady' :
+            return {...state, user :  action.payload, isAuthReady: true}
         default:
             return state
     }
 }
-
-
 // context를 구독할 컴포넌의 묶음 범위를 설정합니다. 
 const AuthContextProvider = ({ children }) => {
+    useEffect(() => {
+        // 로그인 후 새로고침하게 되면 로그인 api 통신하기도 전에 컴포넌트가 불러와져서 logout된 것처럼 보임
+        // 이런 현상을 방지하는 fb 지원 함수가 onAuthStateChange임
+        const unsubscribe = onAuthStateChanged(appAuth, (user) => {
+            dispatch({type : 'isAuthReady', payload: user})
+        })
+        return unsubscribe;
+    }, [])
+
     const [state, dispatch] = useReducer(authReducer, {
-        user: null
+        user: null, 
+        isAuthReady: false,
     })
     
     console.log("user State : ", state);
