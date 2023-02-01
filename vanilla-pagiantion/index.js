52944e02ed5c7346c80b3dea613d909c4c22ad26
greenT-Hee <heehee970322@gmail.com>
2023-02-01 12:08:27 +0900

    :hammer: Add: json 정보 뿌리기

--- vanilla-pagiantion/index.js
+++ vanilla-pagiantion/index.js
@@ -4,9 +4,12 @@ const MAX_Page= 5;
 
 async function followListData() {
   try {
-    const res = await fetch('./data.json');
-    const resJson = await res.json();
-    console.log(resJson);
+    const resJson = await (await fetch('./data.json')).json();
+    noticeData = resJson.chelseaPlayer;
+    console.log(noticeData);
+    if(noticeData.length != 0) {
+      createListHTML(1);
+    }
     
   } catch (err) {
     console.error(err);
@@ -14,3 +17,22 @@ async function followListData() {
 }
 
 followListData();
+
+
+const createListHTML = (selectedPage) => {
+  let dataStart = (selectedPage - 1) * MAX_DATA;
+  const tbody = document.querySelector('tbody');
+  
+  let listHtml = '';
+  for(let i = dataStart; i < dataStart + MAX_DATA; i++) {
+    if(noticeData[i] === null) break;
+    listHtml += `<tr>`;
+    listHtml += `<td>${noticeData[i].name}</td>`;
+    listHtml += `<td>${noticeData[i].nation}</td>`;
+    listHtml += `<td>${noticeData[i].position}</td>`;
+    listHtml += `<td>${noticeData[i].score}</td>`;
+    listHtml += `</tr>`;
+
+    tbody.innerHTML = listHtml;
+  }  
+}
